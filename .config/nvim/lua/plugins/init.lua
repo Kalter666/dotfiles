f5b54ccc64d2b40e1651450ba69202fcd4c09f7d
Iakov Kosiakov <kalterxad34@gmail.com>
2024-06-04 20:47:10 +0300

    feat: add none-ls-autoload

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -332,4 +332,10 @@ return {
       require("grug-far").setup {}
     end,
   },
+  {
+    "zeioth/none-ls-autoload.nvim",
+    event = "BufEnter",
+    dependencies = { "williamboman/mason.nvim", "nvimtools/none-ls.nvim" },
+    opts = {},
+  },
 }
