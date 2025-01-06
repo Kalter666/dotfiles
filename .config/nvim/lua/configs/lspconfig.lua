a06c18a92fdf5d3db089a42a796aaf721ea3f0a2
Iakov Kosiakov <kalterxad34@gmail.com>
2025-01-07 01:29:46 +0300

    OS-X: fix none-ls autoload

--- .config/nvim/lua/configs/lspconfig.lua
+++ .config/nvim/lua/configs/lspconfig.lua
@@ -1,4 +1,3 @@
--- EXAMPLE
 local on_attach = require("nvchad.configs.lspconfig").on_attach
 local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
@@ -78,3 +77,48 @@ lspconfig.vtsls.setup {
   },
 }
 
+lspconfig.rust_analyzer.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  init_options = {
+    initializationOptions = {
+      checkOnSave = true,
+    },
+  },
+  settings = {
+    ["rust-analyzer"] = {
+      inlayHints = {
+        bindingModeHints = {
+          enable = false,
+        },
+        chainingHints = {
+          enable = true,
+        },
+        closingBraceHints = {
+          enable = true,
+          minLines = 25,
+        },
+        closureReturnTypeHints = {
+          enable = "never",
+        },
+        lifetimeElisionHints = {
+          enable = "never",
+          useParameterNames = false,
+        },
+        maxLength = 25,
+        parameterHints = {
+          enable = true,
+        },
+        reborrowHints = {
+          enable = "never",
+        },
+        renderColons = true,
+        typeHints = {
+          enable = true,
+          hideClosureInitialization = false,
+          hideNamedConstructor = false,
+        },
+      },
+    },
+  },
+}
--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -31,7 +31,6 @@ return {
     },
     config = function()
       require "configs.lspconfig"
-      require "configs.lspconfig"
     end, -- Override to setup mason-lspconfig
   },
 
@@ -285,11 +284,6 @@ return {
     "RRethy/vim-illuminate",
   },
   {
-    "mrcjkb/rustaceanvim",
-    version = "^4", -- Recommended
-    lazy = false, -- This plugin is already lazy
-  },
-  {
     "hrsh7th/nvim-cmp",
     opts = function()
       return require "configs.cmp"
@@ -332,10 +326,11 @@ return {
       require("grug-far").setup {}
     end,
   },
+  "williamboman/mason-lspconfig.nvim",
   {
     "zeioth/none-ls-autoload.nvim",
     event = "BufEnter",
     dependencies = { "williamboman/mason.nvim", "nvimtools/none-ls.nvim" },
-    opts = {},
+    opts = { diagnostics = true, formatting = true, code_actions = true, completion = true, hover = true },
   },
 }
