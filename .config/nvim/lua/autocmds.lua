a81cc646a74072bee05c3eb8297cfa87d7207a30
Iakov Kosiakov <kalterxad34@gmail.com>
2026-04-14 22:41:42 +0300

    fix wsl ui duplicate

--- .config/nvim/lua/autocmds.lua
+++ .config/nvim/lua/autocmds.lua
@@ -1,1 +1,17 @@
 require "nvchad.autocmds"
+
+local in_tmux = vim.env.TMUX ~= nil
+local is_wsl = vim.fn.has "wsl" == 1
+
+if in_tmux and is_wsl then
+  local group = vim.api.nvim_create_augroup("tmux_wsl_redraw_fix", { clear = true })
+
+  vim.api.nvim_create_autocmd({ "FocusGained", "VimResume" }, {
+    group = group,
+    callback = function()
+      vim.schedule(function()
+        vim.cmd "redraw!"
+      end)
+    end,
+  })
+end
--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -4,3 +4,11 @@ require "nvchad.options"
 
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
+
+local in_tmux = vim.env.TMUX ~= nil
+local is_wsl = vim.fn.has "wsl" == 1
+
+if in_tmux and is_wsl then
+  -- Neovim 0.12 + tmux on WSL can leave duplicated UI fragments after terminal/tab focus changes.
+  vim.opt.termsync = false
+end
