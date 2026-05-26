b0d8bfb4c991eff9b7ba1212feded475469e470a
Iakov Kosiakov <kalterxad34@gmail.com>
2026-05-26 03:12:10 +0300

    refactor null ls cfg

--- .config/nvim/lua/configs/null-ls.lua
+++ .config/nvim/lua/configs/null-ls.lua
@@ -1,31 +1,79 @@
+---@class NullLsBuiltin
+---@field with fun(opts: table): NullLsBuiltin
+
+---@class NullLsBuiltins
+---@field code_actions table<string, NullLsBuiltin>
+---@field diagnostics table<string, NullLsBuiltin>
+---@field formatting table<string, NullLsBuiltin>
+
+---@class NullLs
+---@field builtins NullLsBuiltins
+---@field setup fun(opts: table)
+
+---@type NullLs
 local null_ls = require "null-ls"
 
+---@type NullLsBuiltins
 local b = null_ls.builtins
-local sources = {
 
-  -- webdev stuff
-  b.formatting.prettier.with { filetypes = { "html", "markdown", "css", "javascript", "typescript", "json" } },
-  b.diagnostics.vacuum.with { filetypes = { "yaml" } },
+---@type NullLsBuiltin[]
+local sources = {
+  -- Web / data
+  b.formatting.prettier.with {
+    filetypes = {
+      "astro",
+      "css",
+      "graphql",
+      "handlebars",
+      "html",
+      "htmlangular",
+      "javascript",
+      "javascriptreact",
+      "json",
+      "json5",
+      "jsonc",
+      "less",
+      "markdown",
+      "markdown.mdx",
+      "scss",
+      "svelte",
+      "typescript",
+      "typescriptreact",
+      "vue",
+      "yaml",
+    },
+  },
+  b.formatting.sqlfluff.with {
+    extra_args = { "--dialect", "postgres" },
+  },
+  b.diagnostics.sqlfluff,
   b.diagnostics.stylelint,
-  b.formatting.buf.with { filetypes = { "proto" } },
-  b.diagnostics.buf.with { filetypes = { "proto" } },
-  -- Luav
+  b.diagnostics.vacuum.with { filetypes = { "yaml" } },
+
+  -- Lua
   b.formatting.stylua,
   b.diagnostics.selene,
 
-  -- python
+  -- Python
   b.diagnostics.mypy,
 
-  -- go
-  b.formatting.gofmt,
+  -- Go
+  b.formatting.goimports,
+  b.formatting.gofumpt,
   b.diagnostics.revive,
 
-  -- other
+  -- C / C++ / Proto
+  b.formatting.clang_format.with { filetypes = { "c", "cpp", "cuda" } },
+  b.formatting.buf.with { filetypes = { "proto" } },
+  b.diagnostics.buf.with { filetypes = { "proto" } },
+
+  -- Shell / CI / containers
+  b.formatting.shfmt.with { filetypes = { "bash", "sh", "zsh" } },
   b.diagnostics.actionlint,
-  b.diagnostics.buf,
   b.diagnostics.hadolint,
+
+  -- Markdown / diagrams
   b.diagnostics.markdownlint,
-  b.diagnostics.sqlfluff,
   b.formatting.d2_fmt,
 }
 
